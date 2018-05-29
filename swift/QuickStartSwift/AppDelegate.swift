98d091aecceb2f7beaa74c0c2db9b542b1040934
Alex Pavlov <alex@rushproject.com>
2018-05-29 11:33:10 -0400

    Implements AppDelegate

--- swift/QuickStartSwift/AppDelegate.swift
+++ swift/QuickStartSwift/AppDelegate.swift
@@ -7,40 +7,43 @@
 //
 
 import UIKit
+import UserNotifications
+import SkyhookContext
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let apiKey = ""
+    var accelerator: SHXAccelerator?
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Here we check if apiKey is initialized. This is not to showcase Skyhook API usage patterns. We expect you to keep api key
+        // hardcoded into program code or plist file. The purpose of the whole if() branch is to alert you that you did not complete
+        // an important configuration step. Feel free to skip to the guard branch right away.
+        
+        if apiKey.isEmpty {
+            
+            // Throwing alert view from AppDelegate is a little bit tricky. For starters, we need a ViewController
+            // instance to put the alert on, but AppDelegate is not a ViewController. We can fetch a reference to ViewController
+            // via 'window' property, but the problem is that at this point of execution the main window is not loaded yet.
+            // To ensure the alert code execution after iOS creates the root ViewController we schedule block execution via main queue.
+
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: "No App Key",
+                                              message: "Please visit my.skyhookwireless.com to create app key, then edit AppDelegate to initialize apiKey variable and rebuild the app",
+                                              preferredStyle: .alert)
+                self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+            }
+        }
+        else {
+            accelerator = SHXAccelerator(key: apiKey)
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (granted, error) in }
+        }
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
